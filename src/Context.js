c2c7cc5c22ca654bb5131cad46f515fbfea4169a
Deeptiman Mallick <deeptiman_mallick@outlook.com>
2021-05-30 01:30:57 +0530

    ADD: state for context and fetch images data

--- src/Context.js
+++ src/Context.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Context = React.createContext();
 
 function ContextProvider({ children }) {
+    const [allPhotos, setAllPhotos] = useState([]);
+
+    useEffect(() => {
+        fetch('https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json')
+            .then(res => res.json())
+            .then(data => setAllPhotos(data));
+    }, []);
+
     return (
-        <Context.Provider value="">
+        <Context.Provider value={{ allPhotos }}>
             {children}
         </Context.Provider>
     );
