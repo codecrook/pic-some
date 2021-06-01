e96b45d26b0f3e7f2a976469078d86822af25ad2
Deeptiman Mallick <deeptiman_mallick@outlook.com>
2021-06-02 00:33:25 +0530

    ADD: Image component and image grid display logic

---
+++ src/components/Image.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Image({ className, img }) {
+    return (
+        <div className={`image-container ${className}`}>
+            <img src={img.url} className="image-grid" alt="display images" />
+        </div>
+    );
+}
