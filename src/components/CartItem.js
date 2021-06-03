ca5f342accbdc05dc3264b41abbe98507b3d3899
Deeptiman Mallick <deeptiman_mallick@outlook.com>
2021-06-04 01:42:35 +0530

    ADD: change delete icon style on hover

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,11 +1,18 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import { Context } from '../Context';
 
 export default function CartItem({ item }) {
     const { removeFromCart } = useContext(Context);
+    const [hovered, setHovered] = useState(false);
+    const iconClassName = hovered ? "ri-delete-bin-fill" : "ri-delete-bin-line";
     return (
         <div className="cart-item" onClick={() => removeFromCart(item.id)}>
-            <i className="ri-delete-bin-line"></i>
+            <i
+                className={iconClassName}
+                onMouseEnter={() => setHovered(true)}
+                onMouseLeave={() => setHovered(false)}
+            >
+            </i>
             <img src={item.url} width="130px" alt="" />
             <p>₹5.99</p>
         </div>
