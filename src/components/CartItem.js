aab1c50d56cd7e39bd8688a3ba7687fe09f91286
Deeptiman Mallick <deeptiman_mallick@outlook.com>
2021-06-04 01:07:40 +0530

    ADD: functionality to calculate total

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Context } from '../Context';
 
 export default function CartItem({ item }) {
+    const { removeFromCart } = useContext(Context);
     return (
-        <div className="cart-item">
+        <div className="cart-item" onClick={() => removeFromCart(item.id)}>
             <i className="ri-delete-bin-line"></i>
             <img src={item.url} width="130px" alt="" />
-            <p>$5.99</p>
+            <p>₹5.99</p>
         </div>
     );
 }
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -4,12 +4,15 @@ import CartItem from '../components/CartItem';
 
 export default function Cart() {
     const { cartItems } = useContext(Context);
+    const totalCost = 5.99 * cartItems.length;
     return (
         <main className="cart-page">
             <h1>Check out</h1>
             {cartItems.map(item => <CartItem key={item.id} item={item} />)}
 
-            <p className="total-cost">Total: </p>
+            <p className="total-cost">
+                Total: {totalCost.toLocaleString('en-IN', { style: 'currency', currency: 'INR' })}
+            </p>
             <div className="order-button">
                 <button>Place Order</button>
             </div>
